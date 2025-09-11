0138eb348fbec88812099842c7a35c9c73ff8f48
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-09-11 22:12:17 +0530

    Product changes

--- src/main/java/com/ecommerce/jwt/entity/Product.java
+++ src/main/java/com/ecommerce/jwt/entity/Product.java
@@ -3,6 +3,7 @@ package com.ecommerce.jwt.entity;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +12,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,24 +28,32 @@ import lombok.NoArgsConstructor;
 public class Product {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "product_seq", allocationSize = 1)
 	private Long productId;
 	private String productName;
+	@Column(length = 2000)
 	private String productDescription;
 	private Double productDiscountedPrice;
 	private Double productActualPrice;
 	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "productImage_key")
+//	private ImageModel productImage;
 	
-	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JoinTable(name = "product_images",
-			joinColumns = {
-					@JoinColumn(name="product_id")
-			},
-			inverseJoinColumns = {
-					@JoinColumn(name="image_id")
-			}
-			)
+//	@JoinTable(name = "product_images",
+//			joinColumns = {
+//					@JoinColumn(name="product_id")
+//			},
+//			inverseJoinColumns = {
+//					@JoinColumn(name="image_id")
+//			}
+//			)
+//	
+//	private Set<ImageModel> productImage;
 	
-	private Set<ImageModel> productImageSet;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "product_id")   // creates FK in image_model
+    private Set<ImageModel> productImageSet;  // <-- MATCH frontend naming
 	
 }
